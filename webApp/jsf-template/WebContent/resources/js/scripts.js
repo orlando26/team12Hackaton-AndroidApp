7de1db36ab6c2159a3e0b7b5cd174e144902dced
Torres Navarro <m.torres.navarro@accenture.com>
2018-03-04 03:51:54 -0600

    add login page

--- webApp/jsf-template/WebContent/resources/js/scripts.js
+++ webApp/jsf-template/WebContent/resources/js/scripts.js
@@ -1,5 +1,8 @@
-$(function(){
-	$('#boton').click(function(){
-		console.log('clicked')
-	});
+$('#loginform').submit(function (event) {
+	console.log(':::::submitting::::');
+	var username = $('#username').val();
+	console.log('username', username);
+	var password = $('#password').val();
+	console.log('password', password);
+	event.preventDefault();
 });
